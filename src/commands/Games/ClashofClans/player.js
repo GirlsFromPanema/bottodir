fb550f33aa0521b3a5a9a34777b552a1d1cc4601
vKxni <Konradtoesch@icloud.com>
2022-02-04 17:59:39 +0100

    cr fix

--- src/commands/Games/ClashofClans/player.js
+++ src/commands/Games/ClashofClans/player.js
@@ -2,17 +2,17 @@
 
 const { SlashCommandBuilder } = require("@discordjs/builders");
 const { CommandInteraction, Permissions, MessageEmbed } = require("discord.js");
-const { Client } = require('clashofclans.js');
-const { stripIndents } = require('common-tags');
+const { Client } = require("clashofclans.js");
+const { stripIndents } = require("common-tags");
 const cocToken = process.env.COC_TOKEN;
 
 const coc = new Client({
-    keys: [cocToken]
+  keys: [cocToken],
 });
 
 module.exports.cooldown = {
-    length: 10000, /* in ms */
-    users: new Set()
+  length: 10000 /* in ms */,
+  users: new Set(),
 };
 
 /**
@@ -20,22 +20,23 @@ module.exports.cooldown = {
  * @param {CommandInteraction} interaction The Command Interaciton
  * @param {any} utils Additional util
  */
-module.exports.run = async (interaction, utils) =>
-{
-    try
-    {
-        const tag = interaction.options.getString("tag");
-        
-        const data = await coc.getPlayer(tag).catch(err => {
-            console.log(err);
-            return { ok: false, status: err.code, name: err.message };
-        })
+module.exports.run = async (interaction, utils) => {
+  try {
+    const tag = interaction.options.getString("tag");
 
-        const embed = new MessageEmbed()
-				.setTitle(`${data.name} - ${data.tag}`)
-				.setThumbnail('https://cdn.discordapp.com/attachments/717460150528639077/751713217096712213/unnamed.png')
-				.setDescription(stripIndents`
-					[Player](https://link.clashofclans.com/en?action=OpenPlayerProfile&tag=${encodeURIComponent(data.tag)})
+    const data = await coc.getPlayer(tag).catch((err) => {
+      console.log(err);
+      return { ok: false, status: err.code, name: err.message };
+    });
+
+    const embed = new MessageEmbed()
+      .setTitle(`${data.name} - ${data.tag}`)
+      .setThumbnail(
+        "https://cdn.discordapp.com/attachments/717460150528639077/751713217096712213/unnamed.png"
+      ).setDescription(stripIndents`
+					[Player](https://link.clashofclans.com/en?action=OpenPlayerProfile&tag=${encodeURIComponent(
+            data.tag
+          )})
 		
 					Name:
 					\`${data.name}\`
@@ -52,25 +53,28 @@ module.exports.run = async (interaction, utils) =>
 					War Stars:
 					\`${data.warStars}\`
 					Troops donated:
-					\`${data.donations || 'None'}\`
+					\`${data.donations || "None"}\`
 					Troops received:
-					\`${data.donationsReceived || 'None'}\`
+					\`${data.donationsReceived || "None"}\`
 				  `);
 
-                  interaction.reply({ embeds: [embed], ephemeral: true })
-    }
-    catch (err)
-    {
-        return Promise.reject(err);
-    }
+    interaction.reply({ embeds: [embed], ephemeral: true });
+  } catch (err) {
+    return Promise.reject(err);
+  }
 };
 
 module.exports.permissions = {
-    clientPermissions: [Permissions.FLAGS.SEND_MESSAGES],
-    userPermissions: [Permissions.FLAGS.SEND_MESSAGES]
+  clientPermissions: [Permissions.FLAGS.SEND_MESSAGES],
+  userPermissions: [Permissions.FLAGS.SEND_MESSAGES],
 };
 
 module.exports.data = new SlashCommandBuilder()
-    .setName("player")
-    .setDescription("Search for a Clash of Clans Player")
-    .addStringOption((option) => option.setName("tag").setDescription("Provide the Player Tag of the Account").setRequired(true))
+  .setName("player")
+  .setDescription("Search for a Clash of Clans Player")
+  .addStringOption((option) =>
+    option
+      .setName("tag")
+      .setDescription("Provide the Player Tag of the Account")
+      .setRequired(true)
+  );
