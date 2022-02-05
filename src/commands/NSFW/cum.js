8d50c2a7d0fe7c452b4bdbb9de2497ca5d379f3c
vKxni <Konradtoesch@icloud.com>
2022-02-05 23:49:23 +0100

    refactor

--- src/commands/NSFW/cum.js
+++ src/commands/NSFW/cum.js
@@ -2,6 +2,7 @@
 
 const { SlashCommandBuilder } = require("@discordjs/builders");
 const { CommandInteraction, Permissions, MessageEmbed } = require("discord.js");
+
 const fetch = require("cross-fetch");
 
 module.exports.cooldown = {
