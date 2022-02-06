55c653408c8c7405e70eab1c91a31c882d1dfbb1
Jacks123459 <boeckerjack@protonmail.com>
2022-02-06 12:57:30 -0600

    Stuff

---
+++ src/commands/Fun/anime.js
@@ -0,0 +1,39 @@
+"use strict"
+
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { CommandInteraction, Permissions, MessageEmbed } = require("discord.js");
+
+module.exports.cooldown = {
+    length: 10000,
+    users: new Set()
+};
+
+/**
+ * @param {CommandInteraction}
+ * @param {any}
+ */
+module.exports.run = async (interaction, utils) =>
+{
+    try
+    {
+        const embed = new MessageEmbed()
+        .setTitle("Get Trolled Loser!!!")
+        .setImage("https://tenor.com/view/rick-roll-rick-ashley-never-gonna-give-you-up-gif-22113173")
+
+        await interaction.reply({ embeds: [embed], ephemeral: false });
+        return;
+    }
+    catch (err)
+    {
+        return Promise.reject(err);
+    }
+};
+
+module.exports.permissions = {
+    clientPermissions: [Permissions.FLAGS.SEND_MESSAGES],
+    userPermissions: [Permissions.FLAGS.SEND_MESSAGES]
+};
+
+module.exports.data = new SlashCommandBuilder()
+    .setName("anime")
+    .setDescription("Gives you random anime gifs")
