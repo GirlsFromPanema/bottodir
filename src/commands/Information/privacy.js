42e76e3081ac998b27ac0169990a6c1f9ff6b04d
vKxni <Konradtoesch@icloud.com>
2022-02-06 15:53:31 +0100

    added pp

---
+++ src/commands/Information/privacy.js
@@ -0,0 +1,39 @@
+"use strict";
+
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { CommandInteraction, Permissions, MessageEmbed } = require("discord.js");
+
+module.exports.cooldown = {
+    length: 90000, /* in ms */
+    users: new Set()
+};
+
+/**
+ * Runs the command.
+ * @param {CommandInteraction} interaction The Command Interaciton
+ * @param {any} utils Additional util
+ */
+module.exports.run = async (interaction, utils) =>
+{
+    try
+    {
+        const embed = new MessageEmbed()
+        .setDescription(`View the privacy policy [here](https://github.com/GirlsFromPanema/bottodir/blob/main/privacy.md)`)
+        .setTimestamp()
+
+        interaction.reply({ embeds: [embed] });
+    }
+    catch (err)
+    {
+        return Promise.reject(err);
+    }
+};
+
+module.exports.permissions = {
+    clientPermissions: [Permissions.FLAGS.SEND_MESSAGES],
+    userPermissions: [Permissions.FLAGS.SEND_MESSAGES]
+};
+
+module.exports.data = new SlashCommandBuilder()
+    .setName("privacy")
+    .setDescription("View the privacy policy of Bottodir");
