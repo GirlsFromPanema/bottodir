ba43144fcb165fac79296efa76590b34d4187b98
vKxni <Konradtoesch@icloud.com>
2022-02-12 18:31:41 +0100

    formatting

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -1,5 +1,53 @@
 "use strict";
 
+
+                                                                                                                                                                                                                                                                                                                            /*
+
+                                        ----- INFORMATION ----- INFORMATION ----- INFORMATION ----- INFORMATION ----- INFORMATION ----- INFORMATION 
+
+                                                                            THIS IS THE TIME SYSTEM OF THE BOT.
+                                                                                    MODIFY AT YOUR OWN RISK.
+
+                                                                - THIS FILE IS ONLY NEEDED FOR INIT. THE TIME UTIL QUERIES.
+
+                                     - SUPPORT IS NOT PROVIDED IF THIS IS MODIFIED/CHANGED/MOVED IN ANY WAY THAT MAY BREAK COMMANDS/EVENTS/FUNCTIONS.
+
+
+
+
+                                                                                                                                                                                                                                                                                                                    */
+
+
+
+                                                                                                                                                                                                                                                                                                                                    /*
+
+
+
+                                                                                %CopyrightBegin%
+
+
+                                                                    Copyright Bottodir 2021. All Rights Reserved.
+
+                                                            Licensed under the Apache License, Version 2.0 (the "License");
+                                                            you may not use this file except in compliance with the License.
+
+                                                                    You may obtain a copy of the License at
+
+                                                                    http://www.apache.org/licenses/LICENSE-2.0
+
+                                                            Unless required by applicable law or agreed to in writing, software
+                                                            distributed under the License is distributed on an "AS IS" BASIS,
+                                                        WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+                                                        See the License for the specific language governing permissions and
+                                                                            limitations under the License.
+
+
+                                                                                    %CopyrightEnd%    
+                                                                                    
+                                                                                    
+                                                                                    
+                                                                        --      last update: 12/2/2022         --                                                                                                                                                                                                       */
+
 const { Permissions, CommandInteraction } = require("discord.js");
 const { getKeyByValue, msToMinAndSec } = require("../util/util.js");
 const { red } = require("colors/safe");
--- src/util/db.js
+++ src/util/db.js
@@ -1,6 +1,6 @@
 "use strict";
 
-                                                                                                                                                                                                                                                                                                                            /*
+                                                                                                                                                                                                                                                        /*
 
                                         ----- INFORMATION ----- INFORMATION ----- INFORMATION ----- INFORMATION ----- INFORMATION ----- INFORMATION 
 
@@ -16,9 +16,7 @@
 
                                                                                                                                                                                                                                                                                                                     */
 
-
-
-                                                                                                                                                                                                                                                                                                                                    /*
+                                                                                                                                                                                                                                                                /*
 
 
 
@@ -47,20 +45,15 @@
                                                                                     
                                                                         --      last update: 12/2/2022         --                                                                                                                                                             */
 
+const { connect, Mongoose } = require("mongoose");
+const { cyan, red, green } = require("colors/safe");
 
+const path = require("path");
+require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
 
-
-
-                                const { connect, Mongoose } = require("mongoose");
-                                const { cyan, red, green } = require("colors/safe");
-
-                                    const path = require('path');
-                                    require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
-
-                                
-                                    console.log(cyan("Connecting to Database . . ."));
-                                    connect(process.env.MONGO_TOKEN).then(() => {
-
-                                console.log(green("Successfully connected to database."));
-
-                            }).catch((err) => console.error(red(err)));
+console.log(cyan("Connecting to Database . . ."));
+connect(process.env.MONGO_TOKEN)
+  .then(() => {
+    console.log(green("Successfully connected to database."));
+  })
+  .catch((err) => console.error(red(err)));
