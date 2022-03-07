8fef5039ac2927c41ecbd4bdec4d26d834febe54
vKxni <Konradtoesch@icloud.com>
2022-03-07 17:56:45 +0100

    fixed message

--- src/commands/Moderation/ban.js
+++ src/commands/Moderation/ban.js
@@ -2,10 +2,10 @@
 
 const { SlashCommandBuilder } = require("@discordjs/builders");
 const {
-  CommandInteraction,
-  Permissions,
-  MessageEmbed,
-  WebhookClient,
+    CommandInteraction,
+    Permissions,
+    MessageEmbed,
+    WebhookClient,
 } = require("discord.js");
 
 // Database queries
@@ -15,8 +15,8 @@ const Guild = require("../../models/Logging/logs");
 const emojis = require("../../../Controller/emojis/emojis");
 
 module.exports.cooldown = {
-  length: 10000 /* in ms */,
-  users: new Set(),
+    length: 10000 /* in ms */ ,
+    users: new Set(),
 };
 
 /**
@@ -24,90 +24,91 @@ module.exports.cooldown = {
  * @param {CommandInteraction} interaction The Command Interaciton
  * @param {any} utils Additional util
  */
-module.exports.run = async (interaction, utils) => {
-  try {
-    const target = interaction.options.getMember("target");
-    const reason =
-      interaction.options.getString("reason") || "No reason provided";
-
-    if (!target) return interaction.reply({ content: "This User is invalid" });
-
-    let bandm = new MessageEmbed()
-      .setColor("RED")
-      .setDescription(
-        `You have been banned from **${interaction.guild.name}**\nModerator: ${interaction.user.tag}\nReason: ${reason}`
-      );
-
+module.exports.run = async(interaction, utils) => {
     try {
-      await target.send({ embeds: [bandm] });
-    } catch (error) {
-      interaction.reply({
-        content: `${emojis.error} | Cannot send messages to this user.\nSuccessfully banned without sending warning through DMs.`,
-        ephemeral: true,
-      });
-      console.log(error);
-      return target.ban({ target });
+        const target = interaction.options.getMember("target");
+        const reason =
+            interaction.options.getString("reason") || "No reason provided";
+
+        if (!target) return interaction.reply({ content: "This User is invalid" });
+
+        let bandm = new MessageEmbed()
+            .setColor("RED")
+            .setDescription(
+                `You have been banned from **${interaction.guild.name}**\nModerator: ${interaction.user.tag}\nReason: ${reason}`
+            )
+            .setTimestamp();
+
+        try {
+            await target.send({ embeds: [bandm] });
+        } catch (error) {
+            interaction.reply({
+                content: `${emojis.success} | Successfully kicked ${target.user.tag}.\n\nFailed to send DMs due to closed direct messages.`,
+                ephemeral: true,
+            });
+            console.log(error);
+            return target.ban({ target });
+        }
+
+        let banmsg = new MessageEmbed()
+            .setColor("GREEN")
+            .setTitle(`${target.user.tag} Banned`)
+            .setDescription(
+                `Banned ${target.user.tag} from ${interaction.guild.name}\nModerator: ${interaction.user.tag}\nReason: ${reason}`
+            )
+            .setTimestamp()
+            .setColor("GREEN")
+            .setThumbnail(interaction.guild.iconURL({ dynamic: true }))
+            .setImage(interaction.guild.iconURL({ dynamic: true }));
+
+        const logs = new MessageEmbed()
+            .setTitle("✅ | User banned")
+            .setDescription(
+                `User: ${target.user.tag}\nModerator: ${interaction.user.tag}\nReason: ${reason}`
+            )
+            .setTimestamp()
+            .setColor("RED");
+
+        // Log Channel
+        const guildQuery = await Guild.findOne({ id: interaction.guild.id });
+
+        if (!guildQuery) return;
+        if (guildQuery) {
+            const webhookid = guildQuery.webhookid;
+            const webhooktoken = guildQuery.webhooktoken;
+
+            const webhookClient = new WebhookClient({
+                id: webhookid,
+                token: webhooktoken,
+            });
+
+            webhookClient.send({ embeds: [logs] });
+        }
+
+        await interaction.reply({ embeds: [banmsg], ephemeral: true });
+        return;
+    } catch (err) {
+        return Promise.reject(err);
     }
-
-    let banmsg = new MessageEmbed()
-      .setColor("GREEN")
-      .setTitle(`${target.user.tag} Banned`)
-      .setDescription(
-        `Banned ${target.user.tag} from ${interaction.guild.name}\nModerator: ${interaction.user.tag}\nReason: ${reason}`
-      )
-      .setTimestamp()
-      .setColor("GREEN")
-      .setThumbnail(interaction.guild.iconURL({ dynamic: true }))
-      .setImage(interaction.guild.iconURL({ dynamic: true }));
-
-    const logs = new MessageEmbed()
-      .setTitle("✅ | User banned")
-      .setDescription(
-        `User: ${target.user.tag}\nModerator: ${interaction.user.tag}\nReason: ${reason}`
-      )
-      .setTimestamp()
-      .setColor("RED");
-
-    // Log Channel
-    const guildQuery = await Guild.findOne({ id: interaction.guild.id });
-
-    if (!guildQuery) return;
-    if (guildQuery) {
-      const webhookid = guildQuery.webhookid;
-      const webhooktoken = guildQuery.webhooktoken;
-
-      const webhookClient = new WebhookClient({
-        id: webhookid,
-        token: webhooktoken,
-      });
-
-      webhookClient.send({ embeds: [logs] });
-    }
-
-    await interaction.reply({ embeds: [banmsg], ephemeral: true });
-    return;
-  } catch (err) {
-    return Promise.reject(err);
-  }
 };
 
 module.exports.permissions = {
-  clientPermissions: [Permissions.FLAGS.ADMINISTRATOR],
-  userPermissions: [Permissions.FLAGS.ADMINISTRATOR],
+    clientPermissions: [Permissions.FLAGS.ADMINISTRATOR],
+    userPermissions: [Permissions.FLAGS.ADMINISTRATOR],
 };
 
 module.exports.data = new SlashCommandBuilder()
-  .setName("ban")
-  .setDescription("Ban a user")
-  .addUserOption((option) =>
-    option
-      .setName("target")
-      .setDescription("Select the User to ban")
-      .setRequired(true)
-  )
-  .addStringOption((option) =>
-    option
-      .setName("reason")
-      .setDescription("Provide a Reason to Ban")
-      .setRequired(true)
-  );
+    .setName("ban")
+    .setDescription("Ban a user")
+    .addUserOption((option) =>
+        option
+        .setName("target")
+        .setDescription("Select the User to ban")
+        .setRequired(true)
+    )
+    .addStringOption((option) =>
+        option
+        .setName("reason")
+        .setDescription("Provide a Reason to Ban")
+        .setRequired(true)
+    );
--- src/commands/Moderation/kick.js
+++ src/commands/Moderation/kick.js
@@ -2,10 +2,10 @@
 
 const { SlashCommandBuilder } = require("@discordjs/builders");
 const {
-  CommandInteraction,
-  Permissions,
-  MessageEmbed,
-  WebhookClient,
+    CommandInteraction,
+    Permissions,
+    MessageEmbed,
+    WebhookClient,
 } = require("discord.js");
 
 // Database queries
@@ -15,8 +15,8 @@ const Guild = require("../../models/Logging/logs");
 const emojis = require("../../../Controller/emojis/emojis");
 
 module.exports.cooldown = {
-  length: 10000 /* in ms */,
-  users: new Set(),
+    length: 10000 /* in ms */ ,
+    users: new Set(),
 };
 
 /**
@@ -24,84 +24,84 @@ module.exports.cooldown = {
  * @param {CommandInteraction} interaction The Command Interaciton
  * @param {any} utils Additional util
  */
-module.exports.run = async (interaction, utils) => {
-  try {
-    const target = interaction.options.getMember("target");
-    const reason =
-      interaction.options.getString("reason") || "No reason provided";
-
-    if (!target) return interaction.reply({ content: "This User is invalid" });
-
-    let kickdm = new MessageEmbed()
-      .setColor("RED")
-      .setDescription(
-        `You have been kicked from **${interaction.guild.name}**.\nReason: ${reason}`
-      );
-
+module.exports.run = async(interaction, utils) => {
     try {
-      await target.send({ embeds: [kickdm] });
-    } catch (error) {
-      interaction.reply({
-        content: `${emojis.error} | Cannot send messages to this user.\nSuccessfully kicked without sending warning through DMs.`,
-        ephemeral: true,
-      });
-      console.log(error);
-      return target.kick({ target });
+        const target = interaction.options.getMember("target");
+        const reason =
+            interaction.options.getString("reason") || "No reason provided";
+
+        if (!target) return interaction.reply({ content: "This User is invalid" });
+
+        let kickdm = new MessageEmbed()
+            .setColor("RED")
+            .setDescription(
+                `You have been kicked from **${interaction.guild.name}**.\nReason: ${reason}`
+            );
+
+        try {
+            await target.send({ embeds: [kickdm] });
+        } catch (error) {
+            interaction.reply({
+                content: `${emojis.success} | Successfully kicked ${target.user.tag}.\n\nFailed to send DMs due to closed direct messages.`,
+                ephemeral: true,
+            });
+            console.log(error);
+            return target.kick({ target });
+        }
+
+        let kickmsg = new MessageEmbed()
+            .setColor("GREEN")
+            .setTitle(`${target.user.tag} Kicked`)
+            .setDescription(
+                `Kicked ${target.user.tag} from ${interaction.guild.name}.\nReason: ${reason}`
+            );
+
+        const logs = new MessageEmbed()
+            .setTitle("✅ | Member kicked")
+            .setDescription(
+                `Kicked ${target.user.tag}\nModerator: ${interaction.user.tag}\nReason: ${reason}`
+            )
+            .setColor("GREEN")
+            .setTimestamp();
+
+        const guildQuery = await Guild.findOne({ id: interaction.guild.id });
+        if (!guildQuery) return;
+        if (guildQuery) {
+            const webhookid = guildQuery.webhookid;
+            const webhooktoken = guildQuery.webhooktoken;
+
+            const webhookClient = new WebhookClient({
+                id: webhookid,
+                token: webhooktoken,
+            });
+
+            webhookClient.send({ embeds: [logs] });
+        }
+
+        await interaction.reply({ embeds: [kickmsg], ephemeral: true });
+        return;
+    } catch (err) {
+        return Promise.reject(err);
     }
-
-    let kickmsg = new MessageEmbed()
-      .setColor("GREEN")
-      .setTitle(`${target.user.tag} Kicked`)
-      .setDescription(
-        `Kicked ${target.user.tag} from ${interaction.guild.name}.\nReason: ${reason}`
-      );
-
-    const logs = new MessageEmbed()
-      .setTitle("✅ | Member kicked")
-      .setDescription(
-        `Kicked ${target.user.tag}\nModerator: ${interaction.user.tag}\nReason: ${reason}`
-      )
-      .setColor("GREEN")
-      .setTimestamp();
-
-    const guildQuery = await Guild.findOne({ id: interaction.guild.id });
-    if (!guildQuery) return;
-    if (guildQuery) {
-      const webhookid = guildQuery.webhookid;
-      const webhooktoken = guildQuery.webhooktoken;
-
-      const webhookClient = new WebhookClient({
-        id: webhookid,
-        token: webhooktoken,
-      });
-
-      webhookClient.send({ embeds: [logs] });
-    }
-
-    await interaction.reply({ embeds: [kickmsg], ephemeral: true });
-    return;
-  } catch (err) {
-    return Promise.reject(err);
-  }
 };
 
 module.exports.permissions = {
-  clientPermissions: [Permissions.FLAGS.ADMINISTRATOR],
-  userPermissions: [Permissions.FLAGS.ADMINISTRATOR],
+    clientPermissions: [Permissions.FLAGS.ADMINISTRATOR],
+    userPermissions: [Permissions.FLAGS.ADMINISTRATOR],
 };
 
 module.exports.data = new SlashCommandBuilder()
-  .setName("kick")
-  .setDescription("Kick a user")
-  .addUserOption((option) =>
-    option
-      .setName("target")
-      .setDescription("Select the User to kick")
-      .setRequired(true)
-  )
-  .addStringOption((option) =>
-    option
-      .setName("reason")
-      .setDescription("Provide a Reason to kick")
-      .setRequired(true)
-  );
+    .setName("kick")
+    .setDescription("Kick a user")
+    .addUserOption((option) =>
+        option
+        .setName("target")
+        .setDescription("Select the User to kick")
+        .setRequired(true)
+    )
+    .addStringOption((option) =>
+        option
+        .setName("reason")
+        .setDescription("Provide a Reason to kick")
+        .setRequired(true)
+    );
